d52ec4e684a43f72c6b1dea7e109c0c97ab00304
Trevor <trevorgschwartz@gmail.com>
2020-04-29 14:29:14 -0400

    axios functions added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios';
+import Axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
@@ -7,10 +8,69 @@ class App extends React.Component {
         this.state = {
             transactions: [],
             categories: [],
-            income: null
+            income: null,
+            transactionByCategory: []
         };
     }
 
+    componentDidMount() {
+      this.getAllCategories(() => {
+        this.getTransactionByCategory()
+      })
+    }
+
+    getAllCategories(callback) {
+      Axios.get('/api/getCategories')
+      .then(results => {
+        // console.log('results', results)
+        let array = results.data;
+        array.sort((a, b) => {
+          return a.id - b.id;
+        })
+        this.setState({
+          categories: array
+        }, () => {
+          console.log('category state', this.state.categories)
+        })
+      })
+      .then(()=> {
+        callback()
+      })
+      .catch(err => {
+        console.log("error getting categories client", err)
+      })
+    }
+
+    getTransactionByCategory() {
+      let transactionArray = [];
+      console.log('category in gettransactionbycategory', this.state.categories)
+      this.state.categories.map(category => {
+        
+        Axios.post('/api/getByCategory', category)
+        .then((result) => {
+          transactionArray.push(result.data)
+        })
+        .then(() => {
+          this.setState({
+            transactionByCategory: transactionArray
+          })
+        })
+        .catch(err => {
+          console.log('error getting TransactionByCategory client', err)
+        })
+    })
+    console.log('transaction Array', transactionArray)
+  }
+
+  saveTransaction() {
+    Axios.post('/api/save', transaction)
+    .then(() => {
+      this.getTransactionByCategory()
+    })
+    .catch(err => {
+      console.log('error saving transaction client', err)
+    })
+  }
 
     render() {
         return (
