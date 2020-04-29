2bcfe5de3f4af7a539e7d48b4c3980e43238a741
Trevor <trevorgschwartz@gmail.com>
2020-04-28 21:31:29 -0400

    database and server implementet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
 import React from "react";
+import axios from 'axios';
 
 class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return <h1>Hello {name}</h1>;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            transactions: [],
+            categories: [],
+            income: null
+        };
+    }
+
+
+    render() {
+        return (
+            <div className="app">
+                <header><h1 className="main">Thrifty</h1></header>
+                <div>
+                    What is your Monthly Net Income?
+                    {/* <input onSubmit ref={input = } type="text" /> */}
+                </div>
+                <div>
+                    <button id="submit">Submit</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
